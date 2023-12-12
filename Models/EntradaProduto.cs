ef2531dd5610414161bc9a7ca36d432f1e59626f
Mxrrega <matheusmarregamarrega@gmail.com>
2023-12-12 13:10:07 -0300

    Adicionar arquivos de projeto.

---
+++ Models/EntradaProduto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginCliente.Models
+{
+    [Table("EntradaProduto")]
+    public class EntradaProduto
+    {
+        [Column("Id")]
+        [Display(Name = "Cód. Entrada Produto")]
+        public int Id { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        [Display(Name = "Produto")]
+        public int ProdutoId { get; set; }
+        public Produto? Produto { get; set; }
+
+        [Column("EntradaProdutoEstoque")]
+        [Display(Name = "Quantidade em estoque")]
+        public int EntradaProdutoEstoque { get; set; }
+        
+
+        [Column("DataEntrada")]
+        [Display(Name = "Data Entrada")]
+        public DateTime DataEntrada { get; set; }
+    }
+}
